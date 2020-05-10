aafd207f585fc707b5d74a2ff029cb09cd484dac
Pedro Costa <pedrof.costa@gmail.com>
2020-05-10 13:41:10 +0100

    Adds scripts to release

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
-RELEASE=0.2.0
-OS=linux
-ARCH=arm
+VERSION=0.2.3
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH_U=$(uname -m)
+if [ "$ARCH_U" == 'x86_64' ];
+then
+  ARCH="amd64"
+fi
 
-wget "https://github.com/costap/tunnel/releases/download/${RELEASE}/tunneld_${OS}_${ARCH}" -o tunneld
-wget "https://github.com/costap/tunnel/releases/download/${RELEASE}/tunnelctl_${OS}_${ARCH}" -o tunnelctl
+if [ "$ARCH_U" == 'armv*' ];
+then
+  ARCH="arm"
+fi
+
+if [ "$ARCH_U" == 'x86_32' ];
+then
+  ARCH="386"
+fi
+
+if [ -z "$ARCH" ]; then echo "$ARCH_U is not supported"; exit 1; fi
+
+wget "https://github.com/costap/tunnel/releases/download/${VERSION}/tunneld_${OS}_${ARCH}" -o tunneld
+wget "https://github.com/costap/tunnel/releases/download/${VERSION}/tunnelctl_${OS}_${ARCH}" -o tunnelctl
 
 install -m 0755 tunnelctl /usr/local/bin
 install -m 0755 tunneld /usr/local/bin
