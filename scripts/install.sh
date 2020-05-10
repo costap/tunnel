14600a75230eb5faaae70379ea266f9e755072ce
Pedro Costa <pedrof.costa@gmail.com>
2020-05-10 14:08:52 +0100

    Fix install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,25 +3,26 @@
 VERSION=0.2.1
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH_U=$(uname -m)
-if [ "$ARCH_U" == 'x86_64' ];
+
+if [[ "$ARCH_U" == x86_64 ]];
 then
   ARCH="amd64"
 fi
 
-if [ "$ARCH_U" == 'armv*' ];
+if [[ "$ARCH_U" == armv* ]];
 then
   ARCH="arm"
 fi
 
-if [ "$ARCH_U" == 'x86_32' ];
+if [[ "$ARCH_U" == x86_32 ]];
 then
   ARCH="386"
 fi
 
 if [ -z "$ARCH" ]; then echo "$ARCH_U is not supported"; exit 1; fi
 
-wget "https://github.com/costap/tunnel/releases/download/${VERSION}/tunneld_${OS}_${ARCH}" -o tunneld
-wget "https://github.com/costap/tunnel/releases/download/${VERSION}/tunnelctl_${OS}_${ARCH}" -o tunnelctl
+curl -L "https://github.com/costap/tunnel/releases/download/${VERSION}/tunneld_${OS}_${ARCH}" -o tunneld
+curl -L "https://github.com/costap/tunnel/releases/download/${VERSION}/tunnelctl_${OS}_${ARCH}" -o tunnelctl
 
 install -m 0755 tunnelctl /usr/local/bin
 install -m 0755 tunneld /usr/local/bin
